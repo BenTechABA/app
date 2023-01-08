d65c608b353a57f5476595988f842468821f627a
BenTechABA <bbosssax2009@gmail.com>
2023-01-08 21:16:04 +0100

    serve

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('<h1>Hello world</h1>'));
+
+app.use('/api/users', require('./routes/users'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server has started on ${PORT}`));
