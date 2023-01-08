f2203f2c51d479bd7cce10585d38cae445395aae
BenTechABA <bbosssax2009@gmail.com>
2023-01-08 21:44:14 +0100

    v

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // @routes GET api/auth
-// @desc 
+// @desc Get logged by user
 // @access Private
 
 router.post('/', (res, req) =>{
