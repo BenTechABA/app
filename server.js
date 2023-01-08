138336cbaad74a37d0721b16c26a6c85b7a3d19c
BenTechABA <bbosssax2009@gmail.com>
2023-01-08 21:23:05 +0100

    sev

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ const app = express();
 app.get('/', (req, res) => res.send('<h1>Hello world</h1>'));
 
 app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server has started on ${PORT}`));
